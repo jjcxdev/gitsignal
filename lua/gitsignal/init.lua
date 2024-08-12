faaf81db509f22e6b8b2e3da52c2bc80cd903542
jjcxdev <justinjchambers@gmail.com>
2024-08-11 21:18:24 -0400

    renamed var buf to buf_id

--- lua/gitsignal/init.lua
+++ lua/gitsignal/init.lua
@@ -44,10 +44,10 @@ M.get_changed_files = function()
 
     -- Get unsaved files from Neovim
     local unsaved_files = {}
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    for _, buf_id in ipairs(vim.api.nvim_list_bufs()) do
         local is_modified = vim.api.nvim_buf_get_option(buf, "modified")
         local filename = vim.api.nvim_buf_get_name(buf)
-        print("Buffer:", buf "filename:", "Modified:", is_modified)
+        print("Buffer ID:", buf_id "filename:", "Modified:", is_modified)
 
         if is_modified then
             table.insert(unsaved_files, filename)
